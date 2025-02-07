49c86f5b00b2043fd03964c6e5014230a9c4b18b
shinch58 <shinch888@gmail.com>
2025-02-08 01:16:10 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-# 打印腳本所在的目錄
-echo $(dirname $0)
+# 安裝依賴
+sudo apt-get update
+sudo apt-get install -y yt-dlp
+sudo apt-get install -y python3-pip
+pip3 install requests
 
-# 安裝所需的 Python 模組（如果還未安裝）
-python3 -m pip install requests yt-dlp
-
-# 檢查並創建 output 資料夾（如果不存在）
-if [ ! -d "./output" ]; then
-    mkdir ./output
-fi
-
-# 運行 yt_m.py 腳本
-python3 scripts/yt_m.py
+# 執行 Python 腳本
+python3 /path/to/your/script.py
