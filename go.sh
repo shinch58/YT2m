22dce0e87139c44615c66751f6e6fa29732b8ed5
shinch58 <shinch888@gmail.com>
2025-02-08 01:05:05 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# 打印腳本所在的目錄
 echo $(dirname $0)
 
 # 安裝所需的 Python 模組（如果還未安裝）
-python3 -m pip install requests
+python3 -m pip install requests yt-dlp
+
+# 檢查並創建 output 資料夾（如果不存在）
+if [ ! -d "./output" ]; then
+    mkdir ./output
+fi
 
 # 運行 yt_m.py 腳本
 python3 scripts/yt_m.py
