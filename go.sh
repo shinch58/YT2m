612d291997a8419c59bf6c39e38bded49e8c7e61
shinch58 <shinch888@gmail.com>
2025-02-07 22:36:58 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+echo "🚀 安裝 Selenium 和 ChromeDriver"
+pip install selenium webdriver-manager
+
 echo "🚀 運行 yt_m.py 解析 YouTube 直播 M3U8"
-
-chmod +x go.sh
-
 python3 scripts/yt_m.py
 
 echo "✅ M3U8 解析完成"
